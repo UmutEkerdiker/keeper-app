686be6f6a9be21b99f28940dcf822a1c88f3bacf
Umut <umutekerdiker@gmail.com>
2022-10-31 14:24:41 +0000

    Updates

---
+++ precache-manifest.9e03046e8d268aa75810352023b57045.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f03e9bb872095580b54",
+    "url": "/keeper-app/static/js/main.4f03e9bb.chunk.js"
+  },
+  {
+    "revision": "ea8298eca45c7ac3ddb9",
+    "url": "/keeper-app/static/js/1.ea8298ec.chunk.js"
+  },
+  {
+    "revision": "6a6139f4d4003c4e2e4e",
+    "url": "/keeper-app/static/js/runtime~main.6a6139f4.js"
+  },
+  {
+    "revision": "c576d965bddb77d7a26988820298c1b4",
+    "url": "/keeper-app/index.html"
+  }
+];
